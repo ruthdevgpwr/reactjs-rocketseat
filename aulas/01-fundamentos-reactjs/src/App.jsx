2df27540e1b35a57545fae1b276a254c30ff3647
Ruth Dantas <ruthdevgp@gmail.com>
2023-02-01 16:10:29 -0300

    criando sidebar

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,19 +1,29 @@
 import Header from './components/Header';
 import { Post } from './Post';
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar';
+
 
 export function App() {
 
   return (
     <>
       < Header />
-      <Post
-        author="Ruth Dantas" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit." 
-      />
-      <Post
-        author="Emerson Bezerra" content="Post muito legal"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Ruth Dantas" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit." 
+          />
+          <Post
+            author="Emerson Bezerra" 
+            content="Post muito legal"
+          />
+        </main>
+      </div>
     </>
   )
 }
