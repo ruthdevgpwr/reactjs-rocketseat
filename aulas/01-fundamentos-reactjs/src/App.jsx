c6ff5494b75e418ab8ce71ea89e0b86bf8914394
Ruth Dantas <ruthdevgp@gmail.com>
2023-02-01 10:26:13 -0300

    utilizando css modules

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,12 @@
+import Header from './components/Header';
 import { Post } from './Post';
+import './styles.css';
 
 export function App() {
 
   return (
     <>
+      < Header />
       <Post
         author="Ruth Dantas" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit." 
