45a933347079b061139e83c494ab1f9c2f566e29
Ruth Dantas <ruthdevgp@gmail.com>
2023-02-01 10:48:04 -0300

    criando header da aplicação

--- aulas/01-fundamentos-reactjs/src/components/Header.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Header.module.css';
 
+import igniteLogo from '../assets/ignite-logo.svg';
+
 export default function Header() {
   return (
-    <header>
-      <strong className={styles.header}>Ignite Feed</strong>
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   )
 }
