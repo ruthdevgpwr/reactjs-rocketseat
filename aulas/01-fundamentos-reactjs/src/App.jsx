449e8f316c6f25234d2cc04440e13270764ead42
Ruth Dantas <ruthdevgp@gmail.com>
2023-02-01 21:40:17 -0300

    iterando no JSX

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -4,6 +4,31 @@ import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from './components/Sidebar';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'http://github.com/ruthdevgpwr.png',
+      name: 'Ruth Dantas',
+      role: 'CTO @RocketSeat'
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋',
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da  Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare',
+      },
+    ],
+    published: new Date('2022-05-03')
+  },
+];
 
 export function App() {
 
@@ -14,14 +39,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Ruth Dantas" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit." 
-          />
-          <Post
-            author="Emerson Bezerra" 
-            content="Post muito legal"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                published={post.published}
+              />
+            )
+          })}
         </main>
       </div>
     </>
